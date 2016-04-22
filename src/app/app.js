32678b9a5d431d6f49fc60cd8952856a1147facb
Vitaliy Kovalenko <kovalenko.v92@gmail.com>
2016-04-22 16:24:41 +0300

    replaced hashHistory with custom history

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Root from './containers/root';
 import ReactDOM from 'react-dom';
-import { hashHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import configureStore from './store/ConfigureStore';
+import { useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 window.onload = function() {
   injectTapEventPlugin();
-  ReactDOM.render(<Root routerHistory={hashHistory} store={configureStore()} />, document.getElementById('app'))
+  ReactDOM.render(<Root routerHistory={appHistory} store={configureStore()} />, document.getElementById('app'))
 }
