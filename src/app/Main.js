a446dffe03ba0e4845f894271ca93f1c4b25b0af
Vladimir Ryabchich <vladimir.ryabchich@gmail.com>
2016-04-22 14:03:32 +0300

    main menu

--- src/app/Main.js
+++ src/app/Main.js
@@ -6,6 +6,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
@@ -13,12 +15,18 @@ import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+
+import { SocialPerson, SocialPeople, ActionHome, ActionList, ImageTune } from 'material-ui/svg-icons';
 
 lightBaseTheme.appBar = {
     height: 50,
   };
 
-const drawerWidth = 320;
+
+const drawerWidth = 320,
+      appTitle = '';
+
 const muiTheme = getMuiTheme(lightBaseTheme);
 
 class Main extends React.Component {
@@ -41,6 +49,13 @@ class Main extends React.Component {
           <AppBar title="My App" onLeftIconButtonTouchTap={this.handleToggle} />
           <Drawer width={drawerWidth} open={this.state.visibleBar} >
             <AppBar title="My App" onLeftIconButtonTouchTap={this.handleToggle} />
+            <Menu>
+              <MenuItem primaryText="Home" leftIcon={<ActionHome />}/>
+              <MenuItem primaryText="Sites" leftIcon={<ActionList />}/>
+              <MenuItem primaryText="Users" leftIcon={<SocialPerson />}/>
+              <MenuItem primaryText="Roles" leftIcon={<SocialPeople />}/>
+              <MenuItem primaryText="Account" leftIcon={<ImageTune />}/>
+            </Menu>
           </Drawer>
         </div>
       </MuiThemeProvider>
