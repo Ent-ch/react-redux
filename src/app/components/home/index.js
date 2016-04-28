d568fca21e06ed9c282b110a5eb04386c1ac0da1
Vladimir Ryabchich <vladimir.ryabchich@gmail.com>
2016-04-28 15:45:58 +0300

    change proportions

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -20,40 +20,41 @@ class Home extends React.Component {
   }
 
   render() {
+    const [xsSize, smSize, mdSize, lgSize] = [12, 6, 6, 6];
     return (
       <div>
         <h2><a href="#/">Home</a></h2>
         <Row>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/users">Users</a></h2>
             <a href="#/users/create">Create User</a><br/>
             <a href="#/users/:id/edit">Modify User</a>
           </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/sites">Sites</a></h2>
             <a href="#/sites/create">Create Site</a><br/>
             <a href="#/sites/:id/edit">Modify Site</a>
           </Col>
         </Row><br/><br/>
         <Row>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/roles">Roles</a></h2>
             <a href="#/roles/create">Create Role</a><br/>
             <a href="#/roles/:id/edit">Modify Role</a>
           </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/accounts">Accounts</a></h2>
             <a href="#/accounts/create">Create Account</a><br/>
             <a href="#/accounts/:id/edit">Modify Account</a>
           </Col>
         </Row><br/><br/>
         <Row>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/permissions">Permissions</a></h2>
             <a href="#/permissions/create">Create Permission</a><br/>
             <a href="#/permissions/:id/edit">Modify Permission</a>
           </Col>
-          <Col xs={12} sm={6} md={3} lg={3}>
+          <Col xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
             <h2><a href="#/global_options">Global Options</a></h2>
             <a href="#/global_options/create">Create Global Options</a><br/>
             <a href="#/global_options/:id/edit">Modify Global Options</a>
