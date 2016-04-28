d636a4139a8da77cbc70d295e2fda81f919a2e2b
Vladimir Ryabchich <vladimir.ryabchich@gmail.com>
2016-04-28 12:34:20 +0300

    Bootstarp init

--- src/app/layouts/main.js
+++ src/app/layouts/main.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500, cyan500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Router, { Link, RouteHandler } from "react-router";
 
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
-
-import { SocialPerson, SocialPeople, ActionHome, ActionList, ImageTune } from 'material-ui/svg-icons';
-
-import {List, ListItem, MakeSelectable} from 'material-ui/List';
-const SelectableList = MakeSelectable(List);
-
-lightBaseTheme.appBar = {
-    height: 50,
-  };
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, ProgressBar} from "react-bootstrap";
+import $ from "jquery";
+import classNames from "classnames";
 
 
-const drawerWidth = 320,
-      appTitle = '';
-
-const muiTheme = getMuiTheme(lightBaseTheme);
 
 class Main extends React.Component {
   constructor(props) {
@@ -38,7 +13,6 @@ class Main extends React.Component {
     this.state = {
       visibleBar: true,
     }
-    // this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleToggle = () => {
@@ -47,41 +21,21 @@ class Main extends React.Component {
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div>
-          <AppBar title="My App" onLeftIconButtonTouchTap={this.handleToggle} />
-          <Drawer width={drawerWidth} open={this.state.visibleBar} >
-            <AppBar title="My App" onLeftIconButtonTouchTap={this.handleToggle} />
+      <div id="wrapper" className="content">
 
-            <ListItem primaryText="Home" leftIcon={<ActionHome />}  href="#/" />
+        <Navbar style={ {margin: 0} }>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Admin</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
 
-            <ListItem primaryText="Users" leftIcon={<SocialPerson />}
-              primaryTogglesNestedList={true}
-              nestedItems={[
-                <ListItem primaryText="Create" href="#/sites" />,
-                <ListItem primaryText="Modify" href="#/get" />,
-              ]}
-            />
-            <ListItem primaryText="Roles" leftIcon={<SocialPeople />}
-              primaryTogglesNestedList={true}
-              nestedItems={[
-                <ListItem primaryText="Create" value="/get" />,
-                <ListItem primaryText="Modify" value="/get" />,
-              ]}
-            />
-            <ListItem primaryText="Account" leftIcon={<ImageTune />}
-              primaryTogglesNestedList={false}
-              nestedItems={[
-                <ListItem primaryText="Modify" value="/get" />,
-              ]}
-            />
-          </Drawer>
-
-          {this.props.children}
-
+        <div id="page-wrapper" className="page-wrapper" ref="pageWrapper" style={{minHeight: this.state.Height}}>
+          Content
         </div>
-      </MuiThemeProvider>
 
+      </div>
 
     );
   }
